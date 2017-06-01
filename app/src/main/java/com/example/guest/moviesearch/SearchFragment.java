e2bd1db1185fa123de9beeede2c299c9ad762b99
Kaya Winn and Xi Xia <kaya@>
2017-06-01 10:01:27 -0700

    fragment on main, button is responding to click

---
+++ app/src/main/java/com/example/guest/moviesearch/SearchFragment.java
@@ -0,0 +1,55 @@
+package com.example.guest.moviesearch;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchFragment extends Fragment implements View.OnClickListener {
+    @Bind(R.id.searchInputText)
+    EditText mSearchInputText;
+
+    @Bind (R.id.searchTextView)
+    TextView mSearchTextView;
+    @Bind (R.id.searchButton)
+    Button mSearchButton;
+
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        ButterKnife.bind(this, view);
+        mSearchButton.setOnClickListener(this);
+        return view;
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSearchButton){
+            Log.d("message", "button clicked");
+        }
+
+    }
+}
